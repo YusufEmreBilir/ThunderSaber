b155005e4e786d33cf32f5e3cfb866e79f10bf8c
3dkarpuz <sonhunger@gmail.com>
2024-07-16 03:14:56 +0300

    Update
    General Update

--- ThunderSaberOS.ino
+++ ThunderSaberOS.ino
@@ -2,7 +2,7 @@
 
 ---------- ThunderSaberOS_V3 By YEB ----------
 
-MPU 6050, DFPlayer ve 60LED/m WS2812b ile çalışması için tasarlanmıştır,
+MPU 6050, DFPlayer mini ve 60LED/m WS2812B ile çalışması için tasarlanmıştır,
 arduino nano ve uno üzerinde test edilmiştir.
 
 Son revizyon: 2024
@@ -26,6 +26,9 @@ Son revizyon: 2024
 #define DEBUG true                  //Seri monitör vs. için debug ayarı.
 
 #define NUM_LEDS 120                //Bıçaktaki LED sayısı.
+#define CHIPSET WS2812B             //LED şerit tipi.
+
+#define STATUS_LED 7                //Durum bildirme ledi.
 #define BLADE_LED_PIN 6             //LED şeritin data pini.
 #define MOTOR_PIN 5                 //Titreşim motorunun pini.
 #define ACTIVATION_BUTTON_PIN 4     //Açma/Kapama butonu pini.
@@ -35,14 +38,15 @@ Son revizyon: 2024
 #define SWING_STOP_TRESHOLD 1       //Sallama rutinini durdurmak için gereken hareketsizlik miktarının hassasiyeti.
 
 
-/*---------- İç Değişkenler ----------*/       //Elle değiştirilmemesi gereken değişkenler.
+/*---------- İç Değişkenler ----------*/       //Elle DEĞİŞTİRİLMEMESİ gereken değişkenler.
 
 byte stationary, moving;
-byte internalVolume, prevInternalVolume;                    
-sensors_event_t a, g, temp;
+byte internalVolume, prevInternalVolume;   
+byte errorCounter = 0;                 
+sensors_event_t accel, gyro, temp;
 unsigned long ignitionMillis, flickerMillis, soundEngineMillis;
 unsigned int randomFlicker = 0; 
-int currentLed1 = 0, currentLed2 = 119;
+int currentLed1 = 0, currentLed2 = NUM_LEDS - 1;
 bool saberIsOn = false, igniting = false, flickered = false;
 
 
@@ -56,9 +60,9 @@ Adafruit_MPU6050 mpu;
 
 /*---------- Parametreler ----------*/    //Belli özellikleri kişiselleştirmek için gelişmiş seçenekler.
 
-byte selectedR = 0;
+byte selectedR = 0;           //
 byte selectedG = 0;           //Bıçak Rengi.
-byte selectedB = 255;
+byte selectedB = 255;         //
 
 byte VOLUME = 15;             //Ses seviyesi, Max: 20
 byte BRIGHTNESS = 10;         //Parlaklık, Max: 255
@@ -71,77 +75,139 @@ byte flickerFreqLimit = 100;  //Bıçağın titreme frekansının üst sınırı
 byte motorPower = 75;         //Hareketsiz durumdayken titreşim motoru gücü, Min: 75, Max: 150
 
 
-void setup() 
+/*---------- Başlangıç Fonksiyonları ----------*/
+
+void setPins()
 {
-    #if DEBUG                 //Debug mod aktifse;
-    Serial.begin(115200);     //seri monitörü başlat.
-    #endif
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(STATUS_LED, OUTPUT);
 
-    FPSerial.begin(9600);
-    if(!df.begin(FPSerial, false))
-    {
-      Serial.println(F("df player hata"));
-    }
-    delay(1000);
-    df.stop();
-    delay(500);
-    df.reset();
-    delay(500);
-    df.volume(VOLUME);
-    internalVolume = VOLUME;
-    prevInternalVolume = internalVolume;
-    pinMode(MOTOR_PIN, OUTPUT);
-    pinMode(ACTIVATION_BUTTON_PIN, INPUT);
-    pinMode(BUSY_PIN, INPUT);
-    led.begin();          
-    led.show();          
-    led.setBrightness(BRIGHTNESS);
-    if(!mpu.begin()) {Serial.println(F("mpu hata"));}
-    mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
-    mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    randomSeed(analogRead(0));
-
+  pinMode(ACTIVATION_BUTTON_PIN, INPUT);
+  pinMode(BUSY_PIN, INPUT);
 }
 
-void loop() 
+void ledInitialize()
+{
+  led.begin();          
+  led.show();          
+  led.setBrightness(BRIGHTNESS);
+  randomSeed(analogRead(0));
+}
+
+void DFPlayerInitialize()
+{
+  FPSerial.begin(9600);
+  if(!df.begin(FPSerial, DEBUG)) 
+  {
+    errorCounter++;
+    #if DEBUG
+    Serial.println(F("DFplayer hata"));
+    #endif
+  }
+  delay(1000);        //DFplayerMini yavaş bir komponenttir, komutlar arası zamana ihtiyaç duyar.  
+  df.stop();          
+  delay(500);         //peşpeşe gönderilen komutlar ya algılanmayacak yada komple senkronu bozacaktır.
+  df.reset();
+  delay(500);
+  df.volume(VOLUME);
+  internalVolume = VOLUME;
+  prevInternalVolume = internalVolume;
+}
+
+void MPUInitialize()
+{
+  if(!mpu.begin()) 
+  {
+    errorCounter++;
+    #if DEBUG
+    Serial.println(F("mpu hata"));
+    #endif
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+}
+
+void catchError()
+{
+  if (errorCounter > 0) 
+  {
+    delay(500);
+    
+    #if DEBUG
+    df.play(405);
+    #else
+    df.play(404);
+    #endif
+
+    while (true)
+    {
+      digitalWrite(STATUS_LED, 0);
+      delay(1000/errorCounter);
+      digitalWrite(STATUS_LED, 1);
+      delay(1000/errorCounter);
+    }
+  }
+}
+
+
+void setup() //MARK:setup
+{
+  #if DEBUG                 //Debug modu aktifse;
+  Serial.begin(115200);     //seri monitörü başlat.
+  #endif
+
+  //Başlangıç rutini:
+  setPins();
+  DFPlayerInitialize();
+  MPUInitialize();
+  ledInitialize();
+  catchError();
+}
+
+
+void loop() //MARK:loop
 {
   activationButtonCheck();
   flicker();
   soundEngine();
 }
 
-void activationButtonCheck()
-{
 
+void activationButtonCheck()  //MARK:ActivationCheck
+{
   if (igniting) {switchBlade(saberIsOn);}
   if (digitalRead(ACTIVATION_BUTTON_PIN) == 0) {return;}
   if (!saberIsOn && !igniting)
   {
-    df.play(1);
-    analogWrite(MOTOR_PIN, 120); //motor başlat, başlatma hızı min: 120
+    df.play(1);     //Normalde Setup dışında delay kullanmak, multitasking yapılan bir kodda,  
+    delay(800);     //mantıklı değil ancak burada zaten bütün donanımın DFplayer'ın çalmasını beklemesi gerekli.
+
+    analogWrite(MOTOR_PIN, 120); //Motor başlat, başlatma hızı min: 120
     saberIsOn = true;
+
+    #if DEBUG
     Serial.println(F("igniting"));
+    #endif
   }
   else if (!igniting)
   {
     df.play(3);
     analogWrite(MOTOR_PIN, 120);
     saberIsOn = false;
+
+    #if DEBUG
     Serial.println(F("retracting"));
+    #endif
   }
   igniting = true;
 }
 
 
-void switchBlade(bool state)
+void switchBlade(bool state)  //MARK:SwitchBlade
 {
   if (millis() - ignitionMillis > ignitionSpeed)
   {
-    // Serial.print(currentLed1);
-    // Serial.print(F("\t"));
-    // Serial.println(currentLed2);
-
     if (state)
     {
       for (int i = 0; i<ledPerStep; i++)   //Igniting
@@ -179,19 +245,27 @@ void switchBlade(bool state)
     if (currentLed1 >= 61)   //ateşleme/geri çekme tamamlandı.
     {
       igniting = false;
-      Serial.println(F("ignited"));
       analogWrite(MOTOR_PIN, motorPower); //motor idle
       currentLed1 = 60;
       currentLed2 = 60;
+
+      #if DEBUG
+      Serial.println(F("ignited"));
+      #endif
+
       return;
     }
     else if (currentLed1 <= -1)
     {
       igniting = false;
-      Serial.println(F("retracted"));
       analogWrite(MOTOR_PIN, 0); //motor durdur
       currentLed1 = 0;
       currentLed2 = NUM_LEDS;
+
+      #if DEBUG
+      Serial.println(F("retracted"));
+      #endif
+
       return;
     }
 
@@ -200,7 +274,7 @@ void switchBlade(bool state)
 }
 
 
-void flicker()
+void flicker()  //MARK:Flicker
 {
   if (!igniting && saberIsOn && millis() - flickerMillis > randomFlicker)
   {
@@ -222,13 +296,13 @@ void flicker()
   }
 }
 
-void soundEngine()
+void soundEngine()  //MARK:SoundEngine
 {
 if (igniting || !saberIsOn) {return;}
 if (millis() - soundEngineMillis < soundEngineFreq) {return;}
-mpu.getEvent(&a, &g, &temp);
-byte sumAccel = abs(a.acceleration.x) + abs(a.acceleration.y) + abs(a.acceleration.z);
-byte sumGyro = abs(g.gyro.x) + abs(g.gyro.y) + abs(g.gyro.z);
+mpu.getEvent(&accel, &gyro, &temp);
+byte sumAccel = abs(accel.acceleration.x) + abs(accel.acceleration.y) + abs(accel.acceleration.z);
+byte sumGyro = abs(gyro.gyro.x) + abs(gyro.gyro.y) + abs(gyro.gyro.z);
 
 if (sumAccel > 17 || sumGyro > 0.4)
 {
@@ -243,10 +317,12 @@ if (sumAccel > 17 || sumGyro > 0.4)
   {
     prevInternalVolume = internalVolume;  //dinamik işitsel geri bildirim (sallama sesi).
     df.volume(internalVolume);
+    analogWrite(MOTOR_PIN, map(internalVolume, VOLUME, 30, motorPower, 255));  //dinamik haptik geri bildirim (titreşim).
+
+    #if DEBUG
     Serial.print("moving,  Volume: ");
     Serial.println(internalVolume);
-
-    analogWrite(MOTOR_PIN, map(internalVolume, VOLUME, 30, motorPower, 255));  //dinamik haptik geri bildirim (titreşim).
+    #endif
   }
 }
 else
@@ -258,10 +334,13 @@ else
   {
     internalVolume = VOLUME;  //sesi eski seviyesine geri getir.
     df.volume(VOLUME);
-    Serial.println("stoppped");
-
     analogWrite(MOTOR_PIN, motorPower);  //titreşim motorunu eski seviyesine geri getir.
+
+    #if DEBUG
+    Serial.println("stoppped");
+    #endif
   }
 }
+
 soundEngineMillis = millis();
 }
